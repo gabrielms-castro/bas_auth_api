afb04f1bd4dc9fd33e6e7183c94eac7f8a329680
gabrielms-castro <gabrielms.castro@gmail.com>
2025-01-10 12:14:40 +0000

    alt app/settings.py: add TIME_ZONE and USE_TZ

--- app/settings.py
+++ app/settings.py
@@ -28,13 +28,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     "rest_framework",
     "rest_framework_simplejwt",
-    
+
     "otp",
     "authentication",
-    
+
 ]
 
 MIDDLEWARE = [
